47667438100a0224dcabbd0a1e4d5458b5d3fcfb
434625142 <434625142@qq.com>
2018-03-20 10:16:03 +0800

    Signed-off-by: 434625142 <434625142@qq.com>

--- setup.py
+++ setup.py
@@ -14,17 +14,28 @@ def get_data_files():
 
 if __name__ == '__main__':
     setup(name='bareditor',
-        version='0.14',
-        url='https://github.com/yxdragon/bareditor.git',
+        version='0.17',
+        url='https://github.com/434625142/bareditor',
         description='interactive python image-processing plugin framework',
         long_description=descr,
-        author='YXDragon',
+        author='yxdragon',
         author_email='yxdragon@imagepy.org',
-        license='BSD 3-clause',
+        
+    classifiers=[  # Optional
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
         packages=setuptools.find_packages(),
         package_data=get_data_files(),
         install_requires=[
-            'scikit-image',
             'wxpython',
             'numba',
             'pillow'
